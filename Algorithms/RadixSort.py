b46f60cc90ac9f77f7279f5ff1e5f2ec8f8bd3a2
Vitorhenriquesilvadesa <vitorhenriquesilvadesa@gmail.com>
2024-11-15 16:30:00 -0300

    Radix Sort.

---
+++ Algorithms/RadixSort.py
@@ -0,0 +1,15 @@
+﻿import numpy as np
+
+def generate_random_array(size, max_value):
+    return np.random.randint(0, max_value, size)
+
+def radix_sort(values: np.ndarray) -> np.ndarray:
+    max_bits = int(values.max()).bit_length()
+
+    for i in range(max_bits):
+        mask = (values >> i) & 1
+        zero_bucket = values[mask == 0]
+        one_bucket = values[mask == 1]
+        values = np.concatenate((zero_bucket, one_bucket))
+
+    return values
---
+++ Algorithms/main.py
@@ -0,0 +1,15 @@
+﻿import time
+from Algorithms.RadixSort import generate_random_array
+from RadixSort import radix_sort
+
+if __name__ == '__main__':
+    test_length = [100, 1000, 10000, 100000, 1000000, 10000000]
+
+    for length in test_length:
+        values = generate_random_array(length, 1 << 31)
+
+        start = time.time()
+        sorted_values = radix_sort(values).tolist()
+        end = time.time()
+
+        print(f'Length: {length} | Time: {(end - start):.4f} seconds')
